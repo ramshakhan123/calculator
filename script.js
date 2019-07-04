53768ae5ccb4bd5cd0f74baeba4c640259146e20
ramshakhan123 <49521382+ramshakhan123@users.noreply.github.com>
2019-07-04 17:06:55 +0500

    added files...

--- script.js
+++ script.js
@@ -2,7 +2,75 @@
 
 function number1() {
     let add1 = document.querySelector(".add").innerHTML;
-    document.querySelector("input").value = add1;
+    document.querySelector("input").value += add1;
     
 }
 
+function number2() {
+    let num2 = document.querySelector(".add2").innerHTML;
+      document.querySelector("input").value += num2;
+}
+
+
+function number3() {
+    let num3 = document.querySelector(".add3").innerHTML;
+    document.querySelector("input").value += num3;
+}
+
+
+function number4() {
+    let num4 = document.querySelector(".add4").innerHTML;
+    document.querySelector("input").value += num4;
+}
+
+
+function number5() {
+    let num5 = document.querySelector(".add5").innerHTML;
+    document.querySelector("input").value += num5;
+}
+
+
+function number6() {
+    let num6 = document.querySelector(".add6").innerHTML;
+    document.querySelector("input").value += num6;
+}
+
+
+function number7() {
+    let num7 = document.querySelector(".add7").innerHTML;
+    document.querySelector("input").value += num7;
+}
+
+
+function number8() {
+    let num8 = document.querySelector(".add8").innerHTML;
+    document.querySelector("input").value += num8;
+}
+
+
+function number9() {
+    let num9 = document.querySelector(".add9").innerHTML;
+    document.querySelector("input").value += num9;
+}
+
+
+function number0() {
+    let num0 = document.querySelector(".add0").innerHTML;
+    document.querySelector("input").value += num0;
+}
+
+function multiplyNumber() {
+    let mult = document.querySelector(".multiply").innerHTML;
+    document.querySelector("input").value += mult;
+}
+
+function addition() {
+    let added = document.querySelector(".addition").innerHTML;
+    document.querySelector("input").value += added;
+}
+
+function equal() {
+    let equalValue = document.querySelector(".backg-color-orange").value;
+    document.querySelector('input').innerHTML == equalValue;
+
+}
